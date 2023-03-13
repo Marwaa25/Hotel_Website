b3c70a69ff3d3e2811c27fd7596974cf33580320
jihan@200107 <jihanelhssisen200107@gmail.com>
2023-03-13 12:09:28 +0100

    image commit

--- database/migrations/2023_03_13_105401_images.php
+++ database/migrations/2023_03_13_105401_images.php
@@ -1,8 +1,8 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Storage;
 
 return new class extends Migration
 {
@@ -11,7 +11,29 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('images', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename');
+            $table->timestamps();
+        });
+
+        // Move existing images to storage
+        $images = DB::table('old_images')->get();
+
+        foreach ($images as $image) {
+            $oldPath = '/path/to/old/images/' . $image->filename;
+            $newPath = '/path/to/new/storage/' . $image->filename;
+
+            Storage::move($oldPath, $newPath);
+
+            DB::table('images')->insert([
+                'filename' => $image->filename,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        Schema::drop('old_images');
     }
 
     /**
@@ -19,6 +41,30 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::create('old_images', function (Blueprint $table) {
+            $table->id();
+            $table->string('filename');
+            $table->timestamps();
+        });
+
+        // Move images back to old location
+        $images = DB::table('images')->get();
+
+        foreach ($images as $image) {
+            $oldPath = '/path/to/old/images/' . $image->filename;
+            $newPath = '/path/to/new/storage/' . $image->filename;
+
+            Storage::move($newPath, $oldPath);
+
+            DB::table('old_images')->insert([
+                'filename' => $image->filename,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+
+        Schema::drop('images');
     }
 };
+
+
