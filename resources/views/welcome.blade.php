54e052374a7a9dbd0564baad8cf6d68703facd62
jihan@200107 <jihanelhssisen200107@gmail.com>
2023-03-15 14:15:09 +0100

    map

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,15 @@
     
 @endsection
 @section('scripts')
-    <div class="container">
-         <div class="newsletter_main">
-            <h1 class="newsletter_taital"><br> Localisation d'hotel cote d'or</h1>
-            <div class="get_quote_bt"><a href="https://goo.gl/maps/bxiSFfvdM6sY2tWd8">Voir sur Google Maps</a></div>
-         </div>
-         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12968.185507123657!2d-5.3053643!3d35.651229!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x37b6f3842f42dcbe!2sInstitut%20sp%C3%A9cialis%C3%A9%20dans%20les%20m%C3%A9tiers%20de%20l&#39;offshoring!5e0!3m2!1sfr!2sma!4v1674471128849!5m2!1sfr!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      </div>
+
+<div class="container">
+    <div class="newsletter_main">
+        <h1><br> Localisation d'hôtel Cote d'or à M'diq, Maroc</h1>
+        <div><a href="https://www.google.com/maps/place/M'diq, Maroc/">Voir sur Google Maps</a></div>
+    </div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6647.077035563694!2d-5.318387674444607!3d35.7375704288864!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12fdacc3ed90d7b1%3A0xd4c4fd52e2d17d1e!2sM&#39;diq%2C%20Maroc!5e0!3m2!1sfr!2sma!4v1678205654086!5m2!1sfr!2sma" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    <p>Av Casablanca lot n°90 M'diq, Maroc se trouve dans la ville de M'diq, Maroc.</p>
+</div>
 @endsection
 
 
