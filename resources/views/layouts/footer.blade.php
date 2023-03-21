60a160b9744e0a07204fd7569279a39b84baf64c
jihan@200107 <jihanelhssisen200107@gmail.com>
2023-03-21 18:07:46 +0100

    footer et slide avis

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,39 +1,39 @@
 <footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <h3>Notre hôtel</h3>
-                <ul>
-                    <li><a href="#">Accueil</a></li>
-                    <li><a href="#">Nos chambres</a></li>
-                    <li><a href="#">Nos services</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">À propos de nous</a></li>
-                    <li><a href="#">Réservation</a></li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <h3>Contact</h3>
-                <ul>
-                    <li><i class="fa fa-map-marker">av casablanca lot n°90 mdiq, 93200 M'diq, Maroc</i></li>
-                    <li><i class="fa fa-phone"></i>0539663219</li>
-                    <li><i class="fa fa-envelope">cotedor@gmail.com</i></li>
-                    <li><i class="fa fa-envelope">cotedor.ma</i></li>
-                </ul>
-            </div>
-            <div class="col-md-4">
-                <h3>Suivez-nous</h3>
-                <ul class="social-icons">
-                    <li><a href="#"><i class="fa fa-facebook">Facebook</i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter">Twitter</i></a></li>
-                    <li><a href="#"><i class="fa fa-instagram">Instagram</i></a></li>
-                </ul>
-            </div>
-        </div>
-        <div>
-            <p>© 2023 Cote d'or Hotel Group </p>
-        </div>
-    </div>
+<section class="footer">
+      <div class="box-container">
+         <div class="box">
+            <h3>Liens Rapides</h3>
+                <a href="#">Accueil</a>
+                <a href="#">Nos chambres</a>
+                <a href="#">Nos services</a>
+                <a href="#">Contact</a>
+                <a href="#">À propos de nous</a>
+                <a href="#">Réservation</a>
+         </div>
+         <div class="box">
+            <h3>Liens Supplémentaires</h3>
+            <a href="#"> <i class="fas fa-angle-right"></i> Poser des questions</a>
+            <a href="#"> <i class="fas fa-angle-right"></i>prestation d'été</a>
+            <a href="#"> <i class="fas fa-angle-right"></i>près de nous</a>
+            <a href="#"> <i class="fas fa-angle-right"></i> Les offres de Cote d'or</a>
+         </div>
+         <div class="box">
+            <h3>Infos de contact</h3>
+            <a href="#"> <i class="fas fa-phone"></i>+212 539 663 219</a>
+            <a href="#"> <i class="fas fa-phone"></i> +212 539 663 232 </a>
+            <a href="#"> <i class="fas fa-envelope"></i>cote.dor@live.fr</a>
+            <a href="#"> <i class="fas fa-map"></i>av casablanca lot n°90 mdiq, 93200 M'diq, Maroc</a>
+         </div>
+         <div class="box">
+            <h3>Suivez-nous</h3>
+            <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         </div>
+      </div>
+      <div class="credit">© 2023<span> Cote d'or Hotel Group </span> | tous les droits sont réservés! </div>
+   </section>
 </footer>
 
 <script src="{{ asset('js/jquery.min.js') }}"></script>
