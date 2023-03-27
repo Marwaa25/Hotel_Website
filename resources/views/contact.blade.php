20a889ebd5412450e0403637891b03a2332ea255
Marwaa25 <siarmarwa@gmail.com>
2023-03-27 17:40:49 +0200

    contact style

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.header')
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.1.4/dist/sweetalert2.all.min.js"></script>
+@vite(['resources/css/contact.css'])
 
 @section('content')
 <script>
@@ -33,20 +34,25 @@
         });
     @endif
 </script>
+<div class="background">
+    <h1>CONTACTEZ-NOUS</h1>
+    
+</div>
+<div class="boxform">
+    <form method="POST" action="/contact">
+        @csrf() 
+        <label for="name">Nom :</label>
+        <input type="text" name="name" id="name" required>
 
-<form method="POST" action="/contact">
-    @csrf() 
-    <label for="name">Nom :</label>
-    <input type="text" name="name" id="name" required>
+        <label for="email">Email :</label>
+        <input type="email" name="email" id="email" required>
 
-    <label for="email">Email :</label>
-    <input type="email" name="email" id="email" required>
+        <label for="message">Message :</label>
+        <textarea name="message" id="message" cols="30" rows="10" required></textarea>
 
-    <label for="message">Message :</label>
-    <textarea name="message" id="message" cols="30" rows="10" required></textarea>
-
-    <button type="submit">Envoyer</button>
-</form>
+        <button type="submit">Envoyer</button>
+    </form>
+</div>
 @if (session('success'))
     <script>
         Swal.fire({
@@ -57,3 +63,42 @@
     </script>
 @endif
 @endsection
+@section('footer')
+        <footer>
+        <section class="footer">
+            <div class="box-container">
+                <div class="box">
+                    <h3>Liens Rapides</h3>
+                        <a href="#">Accueil</a>
+                        <a href="#">Nos chambres</a>
+                        <a href="#">Nos services</a>
+                        <a href="#">Contact</a>
+                        <a href="#">À propos de nous</a>
+                        <a href="#">Réservation</a>
+                </div>
+                <div class="box">
+                    <h3>Liens Supplémentaires</h3>
+                    <a href="#"> <i class="fas fa-angle-right"></i> Poser des questions</a>
+                    <a href="#"> <i class="fas fa-angle-right"></i>prestation d'été</a>
+                    <a href="#"> <i class="fas fa-angle-right"></i>près de nous</a>
+                    <a href="#"> <i class="fas fa-angle-right"></i> Les offres de Cote d'or</a>
+                </div>
+                <div class="box">
+                    <h3>Infos de contact</h3>
+                    <a href="#"> <i class="fas fa-phone"></i>+212 539 663 219</a>
+                    <a href="#"> <i class="fas fa-phone"></i> +212 539 663 232 </a>
+                    <a href="#"> <i class="fas fa-envelope"></i>cote.dor@live.fr</a>
+                    <a href="#"> <i class="fas fa-map"></i>av casablanca lot n°90 mdiq, 93200 M'diq, Maroc</a>
+                </div>
+                <div class="box">
+                    <h3>Suivez-nous</h3>
+                    <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+                    <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+                    <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+                    <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+                </div>
+            </div>
+            <div class="credit">© 2023<span> Cote d'or Hotel Group </span> | tous les droits sont réservés! </div>
+        </section>
+        </footer>
+@endsection()
