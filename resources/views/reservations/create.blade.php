71d96dd14c42895cb60439bd65a92ecfbeec2945
marouaz2 <marouazebda@gmail.com>
2023-03-31 14:52:46 +0000

    en for reservation

--- resources/views/reservations/create.blade.php
+++ resources/views/reservations/create.blade.php
@@ -5,13 +5,13 @@
 @section('content')
 
 <div class="background-image">
-        <h1>Cote d'or M'diq Maroc</h1>
+        <h1>Cote d'or M'diq {{__('Maroc')}}</h1>
         <div class="line"></div>
 </div>
 <div class="part">
-        <h1>COTE D'OR, LE BON CHOIX</h1>
-        <p>Faites votre réservation , dans la chambre qui vous plait à la date qui vous convient.</p>
-        <p>Bon séjour</p>
+        <h1>COTE D'OR, {{__('LE BON CHOIX')}}</h1>
+        <p>{{__('Faites votre réservation , dans la chambre qui vous plait à la date qui vous convient.')}}</p>
+        <p>{{__('Bon séjour')}}</p>
       </div>
 <form action="{{ route('reservations.store') }}" method="POST" id="payment-form">
     @csrf
@@ -48,44 +48,44 @@
     </script>
     
     <div class="form-group">
-        <label for="chambre_id">Chambre</label>
+        <label for="chambre_id">{{__('Chambre')}}</label>
         <select name="chambre_id" id="chambre_id" class="form-control">
             @foreach ($chambres as $chambre)
-                <option value="{{ $chambre->id }}">{{ $chambre->type_de_chambre }} </option>
+                <option value="{{ $chambre->id }}">{{ __($chambre->type_de_chambre) }} </option>
             @endforeach
         </select>
 
     </div>
 
     <div class="form-group">
-        <label for="date_arrivee">Date d'arrivée</label>
+        <label for="date_arrivee">{{__('Date d\'arrivée')}}</label>
         <input type="date" name="date_arrivee" id="date_arrivee" class="form-control" required>
     </div>
 
     <div class="form-group">
-        <label for="date_depart">Date de départ</label>
+        <label for="date_depart">{{__('Date de départ')}}</label>
         <input type="date" name="date_depart" id="date_depart" class="form-control" required>
     </div>
 
     <div class="form-group">
-        <label for="nombre_de_personnes">Nombre de personnes</label>
+        <label for="nombre_de_personnes">{{__('Nombre de personnes')}}</label>
         <input type="number" name="nombre_de_personnes" id="nombre_de_personnes" class="form-control" min="1" required>
     </div>
 
     <div class="form-group">
-        <label for="email">Adresse e-mail</label>
+        <label for="email">{{__('Adresse e-mail')}}</label>
         <input type="email" name="email" id="email" class="form-control" required>
     </div>
 
     <div class="form-group">
-        <label for="card-element">Carte de crédit</label>
+        <label for="card-element">{{__('Carte de crédit')}}</label>
         <div id="card-element" class="form-control">
             <!-- Stripe Element will be inserted here. -->
         </div>
     </div>
     <div class="butt">
-        <button class="btn">Annuler</button>
-        <button type="submit" class="btn" id="submit-button">Réserver</button>
+        <button class="btn">{{__('Annuler')}}</button>
+        <button type="submit" class="btn" id="submit-button">{{__('Réserver')}}</button>
     </div>
 
 </form>
@@ -138,8 +138,8 @@
 <section class="offer mtop" id="services">
     <div class="container">
       <div class="heading">
-        <h5> OFFRES EXCLUSIVEs  </h5>
-        <h3>Bénéficier de nos offres </h3>
+        <h5> {{__('OFFRES EXCLUSIVES')}}  </h5>
+        <h3>{{__('Bénéficier de nos offres')}} </h3>
       </div>
 
       <div class="content grid2 mtop">
@@ -148,16 +148,16 @@
             <img src="/pics/siimple.jpg" alt="">
           </div>
           <div class="right">
-            <h4>CHAMBRE SIMPLE</h4>
+            <h4>{{__('Chambre Simple')}}</h4>
             <div class="rate flex">
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
             </div>
-<p>Grand lit,salle de bain , petit déjeuner inclut , wifi et services </p> 
-           <h5>A partir de 400 MAD/nuit</h5>
+<p>{{__('Grand lit,salle de bain , petit déjeuner inclut , wifi et services')}} </p> 
+           <h5>{{__('À partir de')}} 400 MAD/{{__('nuit')}}</h5>
             <button class="flex1">
-              <span>Voir la disponnibilité</span>
+              <span>{{__('Voir la disponibilité')}}</span>
               <i class="fas fa-arrow-circle-right"></i>
             </button>
           </div>
@@ -167,16 +167,16 @@
             <img src="/pics/douuble.jpg" alt="">
           </div>
           <div class="right">
-            <h4>CHAMBRE DOUBLE</h4>
+            <h4>{{__('Chambre Double')}}</h4>
             <div class="rate flex">
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
             </div>
-            <p>Deux lits twin ,salle de bain , petit déjeuner inclut , wifi et services </p>  
-             <h5>A partir de 500 MAD/nuit</h5>
+            <p>{{__('Deux lits twin ,salle de bain , petit déjeuner inclut , wifi et services')}} </p>  
+             <h5>{{__('À partir de')}} 500 MAD/{{__('nuit')}}</h5>
             <button class="flex1">
-              <span>Voir la disponnibilité</span>
+              <span>{{__('Voir la disponibilité')}}</span>
               <i class="fas fa-arrow-circle-right"></i>
             </button>
           </div>
@@ -186,15 +186,16 @@
             <img src="/pics/triple.jpg" alt="">
           </div>
           <div class="right">
-            <h4>CHAMBRE TRIPLE</h4>
+            <h4>{{__('Chambre Triple')}}</h4>
             <div class="rate flex">
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
             </div>
-            <p>Trois lits simples ,salle de bain , petit déjeuner inclut , wifi et services </p>             <h5>A partir de 600 MAD/nuit</h5>
+            <p>{{__('Trois lits simples ,salle de bain , petit déjeuner inclut , wifi et services')}} </p>             
+            <h5>{{__('À partir de')}} 600 MAD/{{__('nuit')}}</h5>
             <button class="flex1">
-              <span>Voir la disponnibilité</span>
+              <span>{{__('Voir la disponibilité')}}</span>
               <i class="fas fa-arrow-circle-right"></i>
             </button>
           </div>
@@ -204,15 +205,16 @@
             <img src="/pics/quadruple.jpg" alt="">
           </div>
           <div class="right">
-            <h4>CHAMBRE QUADRUPLE</h4>
+            <h4>{{__('Chambre Quadruple')}}</h4>
             <div class="rate flex">
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
             </div>
-            <p>Grand lit et deux lits twin ,salle de bain , petit déjeuner inclut , wifi et services </p>             <h5>A partir de 700 MAD/nuit</h5>
+            <p>{{__('Grand lit et deux lits twin ,salle de bain , petit déjeuner inclut , wifi et services')}} </p>             
+            <h5>{{__('À partir de')}} 700 MAD/{{__('nuit')}}</h5>
             <button class="flex1">
-              <span>Voir la disponnibilité</span>
+              <span>{{__('Voir la disponibilité')}}</span>
               <i class="fas fa-arrow-circle-right"></i>
             </button>
           </div>
@@ -222,16 +224,16 @@
             <img src="/pics/siimple2.jpg" alt="">
           </div>
           <div class="right">
-            <h4>SUITE</h4>
+            <h4>{{__('Suite')}}</h4>
             <div class="rate flex">
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
             </div>
-            <p> Grand lit ,salle de bain , peti salon, petit déjeuner inclut , wifi et services </p>     
-                    <h5>A partir de 800 MAD/nuit</h5>
+            <p> {{__('Grand lit ,salle de bain , petit salon, petit déjeuner inclut , wifi et services ')}}</p>     
+                    <h5>{{__('À partir de')}} 800 MAD/{{__('nuit')}}</h5>
             <button class="flex1">
-              <span>Voir la disponnibilité</span>
+              <span>{{__('Voir la disponibilité')}}</span>
               <i class="fas fa-arrow-circle-right"></i>
             </button>
           </div>
@@ -241,16 +243,16 @@
             <img src="/pics/appart.jpg" alt="">
           </div>
           <div class="right">
-            <h4>APPARTEMENT</h4>
+            <h4>{{__('Appartement')}}</h4>
             <div class="rate flex">
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
               <i class="fas fa-star"></i>
             </div>
-            <p>Deux chambres , une avec un grand lit et l'autre avec deux lits twin , salle de bain , salon , cuisine , petit déjeuner inclut , wifi et services </p>           
-            <h5>A partir de 900 MAD/nuit</h5>
+            <p>{{__('Deux chambres , une avec un grand lit et l\'autre avec deux lits twin , salle de bain , salon , cuisine , petit déjeuner inclut , wifi et services ')}}</p>           
+            <h5>{{__('À partir de')}} 900 MAD/{{__('nuit')}}</h5>
             <button class="flex1">
-              <span>Voir la disponnibilité</span>
+              <span>{{__('Voir la disponibilité')}}</span>
               <i class="fas fa-arrow-circle-right"></i>
             </button>
           </div>
@@ -274,37 +276,37 @@
         <section class="footer">
             <div class="box-container">
                 <div class="box2">
-                    <h3>Liens Rapides</h3>
-                        <a href="#">Accueil</a>
-                        <a href="#">Nos chambres</a>
-                        <a href="#">Nos services</a>
-                        <a href="#">Contact</a>
-                        <a href="#">À propos de nous</a>
-                        <a href="#">Réservation</a>
+                    <h3>{{__('Liens Rapides')}}</h3>
+                    <a href="{{ route('/') }}" class="a1">{{__('Accueil')}}</a>
+                    <a href="{{ route('chambres.index') }}">{{__('Nos chambres')}}</a>
+                    <a href="{{ route('services.index') }}">{{__('Nos services')}}</a>
+                    <a href="{{route('contact.contact')}}">{{__('Contact')}}</a>
+                    <a href="{{ route('hotel.index')}}">{{__('À propos de nous')}}</a>
+                    <a href="{{ route('reservations.create')}}">{{__('Réservation')}}</a>
                 </div>
                 <div class="box2">
-                    <h3>Liens Supplémentaires</h3>
-                    <a href="#"> <i class="fas fa-angle-right"></i> Poser des questions</a>
-                    <a href="#"> <i class="fas fa-angle-right"></i>prestation d'été</a>
-                    <a href="#"> <i class="fas fa-angle-right"></i>près de nous</a>
-                    <a href="#"> <i class="fas fa-angle-right"></i> Les offres de Cote d'or</a>
+                    <h3>{{__('Liens Supplémentaires')}}</h3>
+                    <a href="{{route('contact.contact')}}"> <i class="fas fa-angle-right"></i> {{__('Poser des questions')}}</a>
+                    <a href="{{ route('/') }}"> <i class="fas fa-angle-right"></i>{{__('Service d\'été')}}</a>
+                    <a href="{{ route('/') }}"> <i class="fas fa-angle-right"></i>{{__('Près de nous')}}</a>
+                    <a href="{{ route('/') }}"> <i class="fas fa-angle-right"></i> {{__('Les offres de Cote d\'Or')}}</a>
                 </div>
                 <div class="box2">
-                    <h3>Infos de contact</h3>
+                    <h3>{{__('Infos de contact')}}</h3>
                     <a href="#"> <i class="fas fa-phone"></i>+212 539 663 219</a>
                     <a href="#"> <i class="fas fa-phone"></i> +212 539 663 232 </a>
                     <a href="#"> <i class="fas fa-envelope"></i>cote.dor@live.fr</a>
                     <a href="#"> <i class="fas fa-map"></i>av casablanca lot n°90 mdiq, 93200 M'diq, Maroc</a>
                 </div>
                 <div class="box2">
-                    <h3>Suivez-nous</h3>
+                    <h3>{{__('Suivez-nous')}}</h3>
                     <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
                     <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
                     <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
                     <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
                 </div>
             </div>
-            <div class="credit">© 2023<span> Cote d'or Hotel Group </span> | tous les droits sont réservés! </div>
+            <div class="credit">© 2023<span> Cote d'or Hotel Group </span> | {{__('Tous les droits sont réservés!')}} </div>
         </section>
         </footer>
 
