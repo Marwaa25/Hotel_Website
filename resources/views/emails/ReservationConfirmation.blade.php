a1afc9c4e49c3b3cbcf02af22423edd11fd71fc2
marouaz2 <marouazebda@gmail.com>
2023-05-30 15:59:23 +0100

    modification d'email

--- resources/views/emails/ReservationConfirmation.blade.php
+++ resources/views/emails/ReservationConfirmation.blade.php
@@ -2,8 +2,11 @@
 
 <p>Bonjour {{ $reservation->email }},</p>
 
-<p>Nous avons bien enregistré votre réservation pour la chambre {{ $reservation->chambre_id }} du {{ $reservation->date_arrivee }} au {{ $reservation->date_depart }}.</p>
+<p>Nous avons bien enregistré votre réservation pour {{ $reservation->chambre->type_de_chambre  }} du {{ $reservation->date_arrivee }} au {{ $reservation->date_depart }}.</p>
 
+<p>Nombre de personnes : {{ $reservation->nombre_de_personnes }}</p>
+
+<p>Prix total : {{ $reservation->prix_total }}</p>
 <p>Le paiement a été effectué avec succès.</p>
 
 <p>Merci de votre confiance !</p>
